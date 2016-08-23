2c19679b214b8bb2e12b20c5d45a9dc57f4a1209
Andrew Yunt <andrew.yunt@gmail.com>
2016-08-23 13:58:17 -0700

    Change the way MongoDB source handles doubles

--- src/com/andrewyunt/megaarena/db/MongoDBSource.java
+++ src/com/andrewyunt/megaarena/db/MongoDBSource.java
@@ -14,164 +14,138 @@
  * TO REPRODUCE, DISCLOSE OR DISTRIBUTE ITS CONTENTS, OR TO MANUFACTURE, USE, OR SELL ANYTHING THAT IT MAY DESCRIBE, IN WHOLE OR IN PART.
  */
 package com.andrewyunt.megaarena.db;
-
+ 
 import java.net.UnknownHostException;
 import java.util.Date;
-
+ 
+import com.mongodb.*;
 import org.bukkit.configuration.file.FileConfiguration;
-
+ 
 import com.andrewyunt.megaarena.MegaArena;
 import com.andrewyunt.megaarena.objects.Arena;
 import com.andrewyunt.megaarena.objects.Class;
 import com.andrewyunt.megaarena.objects.GamePlayer;
-import com.mongodb.BasicDBObject;
-import com.mongodb.DB;
-import com.mongodb.DBCollection;
-import com.mongodb.DBObject;
-import com.mongodb.MongoClient;
-
+ 
 public class MongoDBSource extends DatabaseHandler {
-	
-	private MongoClient client;
-	private DB db;
-	private DBCollection playersCollection;
-	
-	@Override
-	public boolean connect() {
-		
-		FileConfiguration config = MegaArena.getInstance().getConfig();
-		
-		String ip = config.getString("database-ip");
-		int port = config.getInt("database-port");
-		
+ 
+    private MongoClient client;
+    private DB db;
+    private DBCollection playersCollection;
+ 
+    @Override
+    public boolean connect() {
+ 
+        FileConfiguration config = MegaArena.getInstance().getConfig();
+ 
+        String ip = config.getString("database-ip");
+        int port = config.getInt("database-port");
+        
         try {
-            client = new MongoClient(ip, port);
-        } catch (UnknownHostException e) {
-            return false;
-        }
+			client = new MongoClient(ip, port);
+		} catch (UnknownHostException e) {
+			return false;
+		}
         
         db = client.getDB(config.getString("database-name"));
         playersCollection = db.getCollection("players");
-        
+ 
         return true;
-	}
-	
-	@Override
-	public void savePlayer(GamePlayer player) {
-
-		String uuid = MegaArena.getInstance().getServer().getOfflinePlayer(player.getName())
-				.getUniqueId().toString();
-		
-		DBObject field = new BasicDBObject("uuid", uuid);
-		DBObject playerObj = playersCollection.findOne(field);
-		DBObject replacementObj = new BasicDBObject("uuid", uuid);
-		
-		Class classType = player.getClassType();
-		
-		if (player.getClassType() != null)
-			replacementObj.put("classtype", classType.toString());
-		
-		replacementObj.put("coins", (double) player.getCoins());
-		replacementObj.put("earnedCoins", (double) player.getEarnedCoins());
-		replacementObj.put("kills", (double) player.getKills());
-		
-		playersCollection.update(playerObj, replacementObj);
-	}
-	
-	@Override
-	public void loadPlayer(GamePlayer player) {
-		
-		String uuid = player.getBukkitPlayer().getUniqueId().toString();
-		
-		DBObject field = new BasicDBObject("uuid", uuid);
-		DBObject playerObj = playersCollection.findOne(field);
-		
-		if (playerObj == null) {
-			playerObj = new BasicDBObject("uuid", uuid);
-			playerObj.put("classtype", null);
-			playerObj.put("coins", 0);
-			playerObj.put("earnedCoins", 0);
-			playerObj.put("kills", 0);
-			playersCollection.insert(playerObj);
-			return;
-		}
-		
-		loadClassType(player);
-		loadCoins(player);
-		loadEarnedCoins(player);
-		loadKills(player);
-		loadLayout(player, player.getClassType());
-	}
-	
-	@Override
-	public void loadClassType(GamePlayer player) {
-		
-		String uuid = player.getBukkitPlayer().getUniqueId().toString();
-		
-		DBObject field = new BasicDBObject("uuid", uuid);
-		DBObject playerObj = playersCollection.findOne(field);
-		
-		if (playerObj.containsField("classtype")) {
-			String classTypeStr = (String) playerObj.get("classtype");
-			
-			if (classTypeStr != null)
-				player.setClassType(Class.valueOf(classTypeStr));
-		}
-	}
-	
-	@Override
-	public void loadCoins(GamePlayer player) {
-		
-		String uuid = player.getBukkitPlayer().getUniqueId().toString();
-		
-		DBObject field = new BasicDBObject("uuid", uuid);
-		DBObject playerObj = playersCollection.findOne(field);
-		
-		if (playerObj.containsField("coins"))
-			player.setCoins((double) playerObj.get("coins"));
-	}
-	
-	@Override
-	public void loadEarnedCoins(GamePlayer player) {
-		
-		String uuid = player.getBukkitPlayer().getUniqueId().toString();
-		
-		DBObject field = new BasicDBObject("uuid", uuid);
-		DBObject playerObj = playersCollection.findOne(field);
-		
-		if (playerObj.containsField("earnedCoins"))
-			player.setEarnedCoins((double) playerObj.get("earnedCoins"));
-	}
-	
-	@Override
-	public void loadKills(GamePlayer player) {
-		
-		String uuid = player.getBukkitPlayer().getUniqueId().toString();
-		
-		DBObject field = new BasicDBObject("uuid", uuid);
-		DBObject playerObj = playersCollection.findOne(field);
-		
-		if (playerObj.containsField("kills"))
-			player.setKills((double) playerObj.get("kills"));
-	}
-
-	@Override
-	public void saveLayout(GamePlayer player, Class classType) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void loadLayout(GamePlayer player, Class classType) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public void saveKill(GamePlayer killer, GamePlayer victim, Date date, Arena arena) {
-		// TODO Auto-generated method stub
-		
-	}
-	
-	//TODO: Add loadLayout method
+    }
+ 
+    @Override
+    public void savePlayer(GamePlayer player) {
+    	
+        String uuid = MegaArena.getInstance().getServer().getOfflinePlayer(player.getName()).getUniqueId().toString();
+        DBObject field = new BasicDBObject("uuid", uuid);
+        DBObject playerObj = playersCollection.findOne(field);
+        DBObject replacementObj = new BasicDBObject("uuid", uuid);
+ 
+        Class classType = player.getClassType();
+ 
+        if (player.getClassType() != null)
+            replacementObj.put("classtype", classType.toString());
+ 
+        replacementObj.put("coins", (double) player.getCoins());
+        replacementObj.put("earnedCoins", (double) player.getEarnedCoins());
+        replacementObj.put("kills", (double) player.getKills());
+ 
+        playersCollection.update(playerObj, replacementObj);
+    }
+ 
+    @Override
+    public void loadPlayer(GamePlayer player) {
+ 
+        String uuid = player.getBukkitPlayer().getUniqueId().toString();
+ 
+        DBObject field = new BasicDBObject("uuid", uuid);
+        DBObject playerObj = playersCollection.findOne(field);
+ 
+        if (playerObj == null) {
+            playerObj = new BasicDBObject("uuid", uuid);
+            playerObj.put("classtype", null);
+            playerObj.put("coins", 0);
+            playerObj.put("earnedCoins", 0);
+            playerObj.put("kills", 0);
+            playersCollection.insert(playerObj);
+            return;
+        }
+ 
+        loadClassType(player);
+        loadCoins(player);
+        loadEarnedCoins(player);
+        loadKills(player);
+        loadLayout(player, player.getClassType());
+    }
+ 
+    @Override
+    public void loadClassType(GamePlayer player) {
+ 
+        String uuid = player.getBukkitPlayer().getUniqueId().toString();
+ 
+        DBObject field = new BasicDBObject("uuid", uuid);
+        DBObject playerObj = playersCollection.findOne(field);
+ 
+        if (playerObj.containsField("classtype")) {
+            String classTypeStr = (String) playerObj.get("classtype");
+ 
+            if (classTypeStr != null)
+                player.setClassType(Class.valueOf(classTypeStr));
+        }
+    }
+ 
+    @Override
+    public void loadCoins(GamePlayer player) {
+ 
+        String uuid = player.getBukkitPlayer().getUniqueId().toString();
+ 
+        DBObject field = new BasicDBObject("uuid", uuid);
+        DBObject playerObj = playersCollection.findOne(field);
+ 
+        if (playerObj.containsField("coins"))
+            player.setCoins(getDouble(playerObj, "coins"));
+    }
+ 
+ 
+ 
+    }
+ 
+    @Override
+    public void saveLayout(GamePlayer player, Class classType) {
+        // TODO Auto-generated method stub
+ 
+    }
+ 
+    @Override
+    public void loadLayout(GamePlayer player, Class classType) {
+        // TODO Auto-generated method stub
+ 
+    }
+ 
+    @Override
+    public void saveKill(GamePlayer killer, GamePlayer victim, Date date, Arena arena) {
+        // TODO Auto-generated method stub
+ 
+    }
+ 
+    //TODO: Add loadLayout method
 }
