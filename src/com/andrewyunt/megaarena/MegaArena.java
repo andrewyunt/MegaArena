2902d56ba2af23fea1f534112839da3b6b0df03d
Andrew Yunt <andrew.yunt@gmail.com>
2016-09-05 01:44:50 -0700

    Add ProtocolLib as a dependency

--- src/com/andrewyunt/megaarena/MegaArena.java
+++ src/com/andrewyunt/megaarena/MegaArena.java
@@ -28,7 +28,6 @@ import org.bukkit.inventory.ItemStack;
 import org.bukkit.inventory.meta.ItemMeta;
 import org.bukkit.plugin.PluginManager;
 import org.bukkit.plugin.java.JavaPlugin;
-import org.bukkit.scheduler.BukkitScheduler;
 
 import com.andrewyunt.megaarena.command.ArenaCommand;
 import com.andrewyunt.megaarena.command.DuelAcceptCommand;
@@ -98,7 +97,7 @@ public class MegaArena extends JavaPlugin {
 	public void onEnable() {
 		
 		/* Check for dependencies */
-		if (pm.getPlugin("StaffPlus") == null) {
+		if (pm.getPlugin("StaffPlus") == null || pm.getPlugin("ProtocolLib") == null) {
 			logger.severe("MegaArena is missing one or more dependencies, shutting down...");
 			pm.disablePlugin(this);
 			return;
